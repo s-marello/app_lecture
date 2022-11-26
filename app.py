84e9825f00ba64c20021c7a665d5a7fd5081923f
s-marello <66569290+s-marello@users.noreply.github.com>
2022-11-26 14:47:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import tensorflow as tf
 app = Flask(__name__)
 
 
-def get_prediction(person):
-    model = tf.keras.models.load_model(r"models")
-    y_pred = model.predict(person)
-
-    return f"Человек выжил с вероятностью {y_pred}"
+def get_prediction(smn, pl, mu, ko, seg, tv, pp, ps, un, shn, pn):
+    model = tf.keras.models.load_model(r"model") 
+    user_result = model.predict(np.array([[smn, pl, mu, ko, seg, tv, pp, ps, un, shn, pn]]))
+    
+    return f"Ответ {user_result}"
 
 
 @app.route('/', methods = ['POST', 'GET'])
@@ -19,13 +19,20 @@ def get_prediction(person):
 def processing():
     message = ''
     if request.method == 'POST':
-        person = request.form.get('username')
+        smn = request.form.get('smn')
+        pl = request.form.get('pl')
+        mu = request.form.get('mu')
+        ko = request.form.get('ko')
+        seg = request.form.get('seg')
+        tv = request.form.get('tv')
+        pp = request.form.get('pp')
+        ps = request.form.get('ps')
+        un = request.form.get('un')
+        shn = request.form.get('shn')
+        pn = request.form.get('pn')
+        
 
-        person_parameters = person.split(" ")
-        person = [float(param) for param in person_parameters]
-        person = np.array([person])
-
-        message = get_prediction(person)
+        message = get_prediction(smn, pl, mu, ko, seg, tv, pp, ps, un, shn, pn)
 
     return render_template('login.html', message=message)
 

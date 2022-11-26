b433375ea54d1c8a945b3b47b7710fc4db0b7ca9
s-marello <66569290+s-marello@users.noreply.github.com>
2022-11-26 21:03:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,19 +18,19 @@ def get_prediction(user_input):
 def processing():
     message = ''
     if request.method == 'POST':
-        smn = request.form.get('smn')
-        pl = request.form.get('pl')
-        mu = request.form.get('mu')
-        ko = request.form.get('ko')
-        seg = request.form.get('seg')
-        tv = request.form.get('tv')
-        pp = request.form.get('pp')
-        ps = request.form.get('ps')
-        un = request.form.get('un')
-        shn = request.form.get('shn')
-        pn = request.form.get('pn')
+        smn = request.form.get('smn'),
+        pl = request.form.get('pl'),
+        mu = request.form.get('mu'),
+        ko = request.form.get('ko'),
+        seg = request.form.get('seg'),
+        tv = request.form.get('tv'),
+        pp = request.form.get('pp'),
+        ps = request.form.get('ps'),
+        un = request.form.get('un'),
+        shn = request.form.get('shn'),
+        pn = request.form.get('pn'),
         
-        sum_model = np.array([[smn, pl, mu, ko, seg, tv, pp, ps, un, shn, pn]])
+        sum_model = np.array([[smn, pl, mu, ko, seg, tv, pp, ps, un, shn, pn]]),
         message = get_prediction(sum_model)
 
     return render_template('login.html', message=message)

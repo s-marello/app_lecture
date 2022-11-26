b86476ffd3440ad43a026c1258f16162cb3c133b
s-marello <66569290+s-marello@users.noreply.github.com>
2022-11-27 02:02:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ import tensorflow as tf
 
 app = Flask(__name__)
 
-@app.route('/')
-def choose_prediction_method():
-    return render_template('main.html')
-
 def get_prediction(user_input):
     model = tf.keras.models.load_model('model') 
     user_result = model.predict(user_input)
     return f"Ответ: {user_result}"
 
+@app.route('/', methods=['POST', 'GET'])
+
 @app.route('/prediction', methods=['POST', 'GET'])
 def prediction():
     message = ''
+    if request.method == 'GET':
+        return render_template('main.html')
     if request.method == 'POST':  
         message = get_prediction(np.array([[request.form.get('smn'), 
                                             request.form.get('pl'),
@@ -28,8 +28,7 @@ def prediction():
                                             request.form.get('un'),
                                             request.form.get('shn'),
                                             request.form.get('pn')]]))
-    return render_template('main.html', message=message)
-
-
+        return render_template('main.html', message=message)
+    
 if __name__ == '__main__':
     app.run()

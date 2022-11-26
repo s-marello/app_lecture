3858a727721586753bf16fc36985a6f0c76038f3
s-marello <66569290+s-marello@users.noreply.github.com>
2022-11-26 21:20:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,22 +17,17 @@ def get_prediction(user_input):
 @app.route('/predict/', methods=['post', 'get'])
 def processing():
     message = ''
-    if request.method == 'POST':
-        smn = request.form.get('smn'),
-        pl = request.form.get('pl'),
-        mu = request.form.get('mu'),
-        ko = request.form.get('ko'),
-        seg = request.form.get('seg'),
-        tv = request.form.get('tv'),
-        pp = request.form.get('pp'),
-        ps = request.form.get('ps'),
-        un = request.form.get('un'),
-        shn = request.form.get('shn'),
-        pn = request.form.get('pn'),
-        
-        sum_model = np.array([[smn, pl, mu, ko, seg, tv, pp, ps, un, shn, pn]]),
-        message = get_prediction(sum_model)
-
+    if request.method == 'POST':  
+        message = get_prediction(np.array([[request.form.get('smn'), 
+                                            request.form.get('pl')
+                                            request.form.get('mu'),
+                                            request.form.get('ko'),
+                                            request.form.get('seg'),
+                                            request.form.get('pp'),
+                                            request.form.get('ps'),
+                                            request.form.get('un'),
+                                            request.form.get('shn'),
+                                            request.form.get('pn')]]))
     return render_template('login.html', message=message)
 
 

ebb937a887bf3d729944a56cee1ca59c66699507
s-marello <66569290+s-marello@users.noreply.github.com>
2022-11-27 02:10:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,17 +17,17 @@ def prediction():
     if request.method == 'GET':
         return render_template('main.html')
     if request.method == 'POST':  
-        message = get_prediction(np.array([[request.form.get('smn'), 
-                                            request.form.get('pl'),
-                                            request.form.get('mu'),
-                                            request.form.get('ko'),
-                                            request.form.get('seg'),
-                                            request.form.get('tv'),
-                                            request.form.get('pp'),
-                                            request.form.get('ps'),
-                                            request.form.get('un'),
-                                            request.form.get('shn'),
-                                            request.form.get('pn')]]))
+        message = 123#get_prediction(np.array([[request.form.get('smn'), 
+                                            #request.form.get('pl'),
+                                            #request.form.get('mu'),
+                                            #request.form.get('ko'),
+                                            #request.form.get('seg'),
+                                            #request.form.get('tv'),
+                                            #request.form.get('pp'),
+                                            #request.form.get('ps'),
+                                            #request.form.get('un'),
+                                            #request.form.get('shn'),
+                                            #request.form.get('pn')]]))
         return render_template('main.html', message=message)
     
 if __name__ == '__main__':
